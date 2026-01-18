c59362b9950d17f38976f90e4efecf54c645915b
SHIWA6 <shivapanday9616527173@gmail.com>
2026-01-18 09:18:29 +0530

    added song

--- Components/UI-comps/Macbook/Apps/Music/data/defaultSongs.ts
+++ Components/UI-comps/Macbook/Apps/Music/data/defaultSongs.ts
@@ -12,14 +12,26 @@
  * These will be loaded into the playlist on first app initialization
  */
 export const defaultYouTubeUrls: string[] = [
+  'https://youtu.be/PsO6ZnUZI0g?si=rfVy122LFmRDGOL_',
+  'https://youtu.be/eVli-tstM5E?si=0gZgy7IiXxNegDCU',
+  'https://youtu.be/YykjpeuMNEk?si=oSOpGrcuC85fGYzA',
+  'https://youtu.be/JGwWNGJdvx8?si=jf6SEZ6PuNOsSg-U',
+  'https://youtu.be/YVkUvmDQ3HY?si=B0WZFWfYc_y0a8Tj',
+  'https://youtu.be/knGCfzm4jWs?si=AcqYlzD2SXmOtlVN',
+   'https://youtu.be/8of5w7RgcTc?si=TEEdfJQb9tl6UPDF',
+   'https://youtu.be/XbGs_qK2PQA?si=4Fdo9nGSbYozmcpp',
+   'https://youtu.be/AbkEmIgJMcU?si=IlyH972nXqopEEFF',
+   'https://youtu.be/IltsOcCj1Ak?si=ikxlRaBtfKWfVVvO',
+
+
   'https://youtu.be/c9X74V2EPew?si=MqBnJux6_mwhqj_F', 
-  'https://youtu.be/nqmRpOMfsyc?si=ZP1uT8m8ISdjur_s', // Song 2 - Replace with your URL
-  'https://youtu.be/PWkYxHRWP3k?si=gzRvraW7KXmFEmUK', // Song 3 - Replace with your URL
-  'https://youtu.be/6iE3r-ebn08?si=-qijoFC4q7XAV6o5', // Song 4 - Replace with your URL
-  'https://youtu.be/PWkYxHRWP3k?si=0csYSOA3Y-UC8r0a', // Song 5 - Replace with your URL
-  'https://youtu.be/iqp-qq8h8uc?si=pZvVMNqBBGAM6kfb', // Song 6 - Replace with your URL
-  'https://youtu.be/nqmRpOMfsyc?si=WKFY2ZILTJz7Y02V', // Song 7 - Replace with your URL
-  'https://youtu.be/7ltVttIHwwI?si=LGl7Nb7pNp4FylXb', // Song 8 - Replace with your URL
-  'https://youtu.be/1_R8-iBj_J0?si=DxOZoL9YgQNe0TuA', // Song 9 - Replace with your URL
-  'https://youtu.be/gnaruIk5T-4?si=y1tSnrK9IOWKv8NL', // Song 10 - Replace with your URL
+  'https://youtu.be/nqmRpOMfsyc?si=ZP1uT8m8ISdjur_s',
+  'https://youtu.be/PWkYxHRWP3k?si=gzRvraW7KXmFEmUK', 
+  'https://youtu.be/6iE3r-ebn08?si=-qijoFC4q7XAV6o5',
+  'https://youtu.be/PWkYxHRWP3k?si=0csYSOA3Y-UC8r0a', 
+  'https://youtu.be/iqp-qq8h8uc?si=pZvVMNqBBGAM6kfb', 
+  'https://youtu.be/nqmRpOMfsyc?si=WKFY2ZILTJz7Y02V', 
+  'https://youtu.be/7ltVttIHwwI?si=LGl7Nb7pNp4FylXb', 
+  'https://youtu.be/1_R8-iBj_J0?si=DxOZoL9YgQNe0TuA', 
+  'https://youtu.be/gnaruIk5T-4?si=y1tSnrK9IOWKv8NL',
 ];
